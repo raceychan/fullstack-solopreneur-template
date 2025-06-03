f2c23e8bac3676bae2796292023018b5f0321d8e
raceychan <raceychan@gmail.com>
2025-06-03 20:51:52 +0800

    wip(backend): auth

--- backend/src/service/auth.py
+++ backend/src/service/auth.py
@@ -1,8 +1,11 @@
-from gotrue.types import SignInWithEmailAndPasswordCredentials
-from lihil import Annotated, Form, Route
+from datetime import datetime
+from typing import Any
+
+from gotrue.types import SignInWithEmailAndPasswordCredentials, User
+from lihil import Annotated, Payload, Route
 from lihil.config import lhl_get_config
 from lihil.plugins.auth.jwt import JWTAuthParam, JWTAuthPlugin
-from lihil.plugins.auth.oauth import OAuth2PasswordFlow, OAuthLoginForm
+from lihil.plugins.auth.oauth import OAuth2PasswordFlow, OAuth2Token, OAuthLoginForm
 from src.config import ProjectConfig
 from supabase import AsyncClient
 
@@ -12,8 +15,10 @@ me = Route("me")
 
 def jwt_decode(graph, func, sig):
     config = lhl_get_config(config_type=ProjectConfig)
-    jwt_auth = JWTAuthPlugin(jwt_secret=config.JWT_SECRET, jwt_algorithms="HS256", verify_aud=False)
-    return jwt_auth.decode_plugin(graph, func, sig)
+    jwt_auth = JWTAuthPlugin(
+        jwt_secret=config.JWT_SECRET, jwt_algorithms="HS256", verify_aud=False
+    )
+    return jwt_auth.decode_plugin(audience="authenticated")(graph, func, sig)
 
 
 @tokens.post
@@ -28,11 +33,24 @@ async def login_get_token(
     if not resp.session:
         raise Exception("no session")
 
-    return resp.session.access_token
+    return OAuth2Token(resp.session.access_token, resp.session.expires_in)
+
+
+class PublicUser(Payload):
+    email: str
+    last_login: datetime
+
+    @classmethod
+    def from_user_dict(cls, user_dict: dict[str, Any]):
+        email = user_dict["email"]
+        last_login = datetime.fromtimestamp(float(user_dict["iat"]))
+
+        return cls(email, last_login)
 
 
 @me.get(auth_scheme=OAuth2PasswordFlow(token_url="token"), plugins=[jwt_decode])
-async def get_user(access_token: Annotated[str, JWTAuthParam]): 
-    ""
-    # assert profile.role == "user"
-    # return User(name="user", email="user@email.com")
+async def get_user(
+    user_dict: Annotated[dict[str, Any] | None, JWTAuthParam] = None,
+) -> PublicUser:
+    assert user_dict
+    return PublicUser.from_user_dict(user_dict)
